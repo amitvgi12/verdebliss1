f9f75f8a2b3eafbce5543f34a1414b0d231b29a6
amitvgi12 <amitsharma.vgi@gmail.com>
2026-05-10 16:15:47 +0530

    sentry removed

--- sentry.edge.config.ts
+++
@@ -1,8 +0,0 @@
-import * as Sentry from '@sentry/nextjs'
-
-Sentry.init({
-  dsn: process.env.SENTRY_DSN,
-  environment: process.env.SENTRY_ENVIRONMENT ?? process.env.VERCEL_ENV ?? 'development',
-  release: process.env.VERCEL_GIT_COMMIT_SHA ?? undefined,
-  tracesSampleRate: process.env.NODE_ENV === 'production' ? 0.05 : 1.0,
-})
